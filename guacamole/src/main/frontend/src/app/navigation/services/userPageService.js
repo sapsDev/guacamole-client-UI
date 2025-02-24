6a608a88d0e699e84c02ea67a1394c94762e6aea
msteiner2001 <m.steiner2001@web.de>
2025-02-24 23:38:47 +0100

    refactor: generateMainPages()

--- guacamole/src/main/frontend/src/app/navigation/services/userPageService.js
+++ guacamole/src/main/frontend/src/app/navigation/services/userPageService.js
@@ -482,7 +482,9 @@ angular.module('navigation').factory('userPageService', ['$injector',
         }
         
         //Add watch page to the list of main pages
-        pages.push(watchPage);
+        if (watchPage) {
+            pages.push(watchPage);
+        }
 
         console.log("[at generateMainPages(): var pages] = ");
         pages.forEach(obj => console.log(JSON.stringify(obj, null, 2)));
