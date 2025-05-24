42c1fa712e92aa2b23baef746245bc17644d8fc6
msteiner2001 <m.steiner2001@web.de>
2025-05-24 17:29:56 +0200

    add: watchSessionService functions to manipulate API watch sessions

--- guacamole/src/main/frontend/src/app/rest/services/watchSessionService.js
+++ guacamole/src/main/frontend/src/app/rest/services/watchSessionService.js
@@ -72,6 +72,118 @@ angular.module('rest').factory('watchSessionService', ['$injector',
 
         };
 
+        /**
+         * Makes a request to the REST API to get the list of watch sessions
+         * returning a promise that provides a map of @link{WatchSessions}
+         * objects if successful.
+         *
+         * @param {String[]} [permissionTypes]
+         *     The set of permissions to filter with. A user must have one or more
+         *     of these permissions for a watch session to appear in the result
+         *     If null, no filtering will be performed. Valid values are listed
+         *     within PermissionSet.ObjectType.
+         *
+         * @returns {Promise.<Object.<String, WatchSession>>}
+         *     A promise which will resolve with a map of @link{WatchSession}
+         *     objects, where each key is the identifier of the corresponding
+         *     watch session.
+         */
+        service.getWatchSessions = function getWatchSessions(dataSource, permissionTypes) {
+
+            // Add permission filter if specified
+            var httpParameters = {};
+            if (permissionTypes)
+                httpParameters.permission = permissionTypes;
+
+            // Retrieve watch sessions
+            return authenticationService.request({
+                method  : 'GET',
+                url     : 'api/session/data/' + encodeURIComponent(dataSource) + '/watchSessions',
+                params  : httpParameters
+            });
+
+        };
+
+        /**
+         * Makes a request to the REST API to delete a watch session, returning a promise
+         * that can be used for processing the results of the call.
+         *
+         * @param {String} dataSource
+         *     The unique identifier of the data source containing the watch session 
+         *     to be deleted. This identifier corresponds to an AuthenticationProvider
+         *     within the Guacamole web application.
+         *
+         * @param {WatchSession} watchSession
+         *     The watch session to delete.
+         *
+         * @returns {Promise}
+         *     A promise for the HTTP call which will succeed if and only if the
+         *     delete operation is successful.
+         */
+        service.deleteWatchSession = function deleteWatchSession(dataSource, watchSession) {
+
+            // Delete watch session
+            return authenticationService.request({
+                method  : 'DELETE',
+                url     : 'api/session/data/' + encodeURIComponent(dataSource) + '/watchSessions/' + encodeURIComponent(watchSession.identifier)
+            })
+            
+        };
+
+        /**
+         * Makes a request to the REST API to create a watch session, returning
+         * a promise that can be used for processing the results of the call.
+         *
+         * @param {String} dataSource
+         *     The unique identifier of the data source in which the watch session
+         *     should be created. This identifier corresponds to an 
+         *     AuthenticationProvider within the Guacamole web application.
+         *
+         * @param {WatchSession} watchSession
+         *     The watch session to create.
+         *
+         * @returns {Promise}
+         *     A promise for the HTTP call which will succeed if and only if the
+         *     create operation is successful.
+         */
+        service.createWatchSession = function createWatchSession(dataSource, watchSession) {
+
+            // Create watch session
+            return authenticationService.request({
+                method  : 'POST',
+                url     : 'api/session/data/' + encodeURIComponent(dataSource) + '/watchSessions',
+                data    : watchSession
+            })
+            
+        };
+
+        /**
+         * Makes a request to the REST API to save a watch session, returning a promise
+         * that can be used for processing the results of the call.
+         *
+         * @param {String} dataSource
+         *     The unique identifier of the data source containing the watch session to
+         *     be updated. This identifier corresponds to an AuthenticationProvider
+         *     within the Guacamole web application.
+         *
+         * @param {WatchSession} watchSession
+         *     The watch session to update.
+         *
+         * @returns {Promise}
+         *     A promise for the HTTP call which will succeed if and only if the
+         *     save operation is successful.
+         */
+        service.saveWatchSession = function saveWatchSession(dataSource, watchSession) {
+
+            // Update watch session
+            return authenticationService.request({
+                method  : 'PUT',
+                url     : 'api/session/data/' + encodeURIComponent(dataSource) + '/watchSessions/' + encodeURIComponent(watchSession.identifier),
+                data    : watchSession
+            })
+
+        };
+
         return service;
 
     }]);
