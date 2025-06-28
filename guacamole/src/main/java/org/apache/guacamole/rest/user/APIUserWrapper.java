d7cace2474822f791433b88e067205a470e76cb6
msteiner2001 <m.steiner2001@web.de>
2025-06-28 23:14:27 +0200

    add: extended APIUserWrapper.java

--- guacamole/src/main/java/org/apache/guacamole/rest/user/APIUserWrapper.java
+++ guacamole/src/main/java/org/apache/guacamole/rest/user/APIUserWrapper.java
@@ -112,6 +112,11 @@ public class APIUserWrapper implements User {
     public ObjectPermissionSet getSharingProfilePermissions() throws GuacamoleException {
         throw new GuacamoleUnsupportedException("APIUserWrapper does not provide permission access.");
     }
+    
+    @Override
+    public ObjectPermissionSet getWatchSessionPermissions() throws GuacamoleException {
+        throw new GuacamoleUnsupportedException("APIUserWrapper does not provide permission access.");
+    }
 
     @Override
     public ObjectPermissionSet getUserPermissions()
