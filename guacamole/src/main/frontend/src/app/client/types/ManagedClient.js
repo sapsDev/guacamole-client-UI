bf7d0902e3d422ecfaf25f23176954fb125e3e66
msteiner2001 <m.steiner2001@web.de>
2025-11-04 23:00:05 +0100

    fix: typo

--- guacamole/src/main/frontend/src/app/client/types/ManagedClient.js
+++ guacamole/src/main/frontend/src/app/client/types/ManagedClient.js
@@ -1088,7 +1088,7 @@ angular.module('client').factory('ManagedClient', ['$rootScope', '$injector',
         console.log(' try watchSessionCreate(identifier: ' + null
             + ',username: ' + username 
             + ',connection: ' + client.name
-            + ',uuid: ' + lient.tunnel.uuid    
+            + ',uuid: ' + client.tunnel.uuid    
             + ',restriction: ' + !!isReadOnly 
             + ',link: ' + link + ')');
         watchSessionService.createWatchSession(
