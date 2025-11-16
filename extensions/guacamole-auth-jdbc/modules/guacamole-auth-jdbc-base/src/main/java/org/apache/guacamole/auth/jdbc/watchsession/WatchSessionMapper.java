b70853bd7386d2cf257846a857a375e7706c8244
msteiner2001 <m.steiner2001@web.de>
2025-11-16 13:55:58 +0100

    add: watch session deletion by tunnel uuid

--- extensions/guacamole-auth-jdbc/modules/guacamole-auth-jdbc-base/src/main/java/org/apache/guacamole/auth/jdbc/watchsession/WatchSessionMapper.java
+++ extensions/guacamole-auth-jdbc/modules/guacamole-auth-jdbc-base/src/main/java/org/apache/guacamole/auth/jdbc/watchsession/WatchSessionMapper.java
@@ -25,4 +25,18 @@ import org.apache.guacamole.auth.jdbc.base.ModeledDirectoryObjectMapper;
  * Mapper for watch session objects.
  */
 public interface WatchSessionMapper
-        extends ModeledDirectoryObjectMapper<WatchSessionModel> {}
+        extends ModeledDirectoryObjectMapper<WatchSessionModel> {
+        
+    /**
+     * Deletes the watch session associated with the given tunnel uuid
+     * If no such watch session exists, this operation has no effect.
+     *
+     * @param uuid
+     *     The tunnel uuid of the watch session to delete.
+     *
+     * @return
+     *     The number of rows deleted.
+     */
+    int deleteOneByUuid(@Param("uuid") String uuid);
+    
+    }
