470e80b63e01511476d181bd7bbfea0f3048c22c
msteiner2001 <m.steiner2001@web.de>
2025-12-11 22:36:46 +0100

    add: logging to find error [1]

--- guacamole/src/main/frontend/src/app/client/controllers/clientController.js
+++ guacamole/src/main/frontend/src/app/client/controllers/clientController.js
@@ -905,15 +905,16 @@ angular.module('client').controller('clientController', ['$scope', '$routeParams
 
         // If there is at least one sharing profile and the right, to create watch sessions, 
         // the connection can be shared automatically with lecturers
-        //dataSourceService.apply(
-        //    permissionService.getEffectivePermissions,
-        //    authenticationService.getDataSource(),
-        //    authenticationService.getCurrentUsername()
-        //).then(function permissionsReceived(permissions) {
-        //    return permissions.systemPermissions.contains("CREATE_WATCH_SESSION") && $scope.canShareConnection();
-        //})
-        console.log('datasource: ' + authenticationService.getDataSource());
-        console.log('username' + authenticationService.getCurrentUsername());
+        dataSourceService.apply(
+            permissionService.getEffectivePermissions,
+            authenticationService.getDataSource(),
+            authenticationService.getCurrentUsername()
+        ).then(function permissionsReceived(permissions) {
+            //return permissions.systemPermissions.contains("CREATE_WATCH_SESSION") && $scope.canShareConnection();
+            console.log('Permissions:' + permissions);
+        })
+        //console.log('datasource: ' + authenticationService.getDataSource());
+        //console.log('username' + authenticationService.getCurrentUsername());
 
     };
 
